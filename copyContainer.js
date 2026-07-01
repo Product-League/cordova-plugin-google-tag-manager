516a0585e046869d77bb9af53cbecba4a7b137c5
apalmaPL <apalma@product-league.com>
2026-07-01 19:03:00 +0200

    Rename copyContainer.js to hooks/copyContainer.js

--- copyContainer.js
+++
@@ -1,2 +0,0 @@
-// TODO: implement copying container folder preserving path for Android/iOS.
-module.exports=function(ctx){console.log("Copy GTM container hook placeholder");};
---
+++ hooks/copyContainer.js
@@ -0,0 +1,2 @@
+// TODO: implement copying container folder preserving path for Android/iOS.
+module.exports=function(ctx){console.log("Copy GTM container hook placeholder");};
