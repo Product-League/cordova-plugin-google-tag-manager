09e35d8da6e680d844e5fc86f0083cc4ceb81df5
apalmaPL <apalma@product-league.com>
2026-07-01 19:22:57 +0200

    Update copyContainer.js

--- hooks/copyContainer.js
+++ hooks/copyContainer.js
@@ -1,2 +1,27 @@
-// TODO: implement copying container folder preserving path for Android/iOS.
-module.exports=function(ctx){console.log("Copy GTM container hook placeholder");};
+
+const fs = require('fs'),
+      path = require('path');
+
+const configs = {
+    androidPath: "/platforms/android/app/src/main/assets/www/",
+    androidMainPath: "/platforms/android/app/src/main/",
+    androidAppPath: "/platforms/android/app/",
+    configPathAndroid: "/platforms/android/app/src/main/res/xml/config.xml",
+    configPathIos: "/platforms/ios/ECOP Mobile TeamC/config.xml",
+    androidManifest: "AndroidManifest.xml",
+    iosPath: "/platforms/ios/www/",
+    iosMainPath: "/platforms/ios/"
+};
+
+module.exports=function(ctx){
+  console.log("Copy GTM container hook placeholder");
+
+  fs.copyFileSync("/resources/container/GTM-KLBN64W6.json", "/platforms/android/app/src/main/assets/containers/GTM-KLBN64W6.json");
+  fs.copyFileSync("/resources/container/GTM-KLBN64W6.json", "/platforms/ios/container/GTM-KLBN64W6.json");
+  
+  
+};
+
+function moveGSFiles(oldPath, newPath){
+    fs.copyFileSync(oldPath, newPath);
+  }
